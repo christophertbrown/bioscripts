1c83c8cbc089d35775b95b8f0c0a28644637d2c0
Chris Brown <ctb@ctb-mbp.local>
2019-01-01 11:10:52 -0800

    added sorting to besthits.py

--- ctbBio/besthits.py
+++ ctbBio/besthits.py
@@ -22,7 +22,41 @@ def top_hits(hits, num, column, reverse):
     for hit in hits[0:num]:
         yield hit
 
-def numBlast(blast, numHits, evalueT = False, bitT = False):
+def numBlast_sort(blast, numHits, evalueT, bitT):
+    """
+    parse b6 output with sorting
+    """
+    header = ['#query', 'target', 'pident', 'alen', 'mismatch', 'gapopen',
+              'qstart', 'qend', 'tstart', 'tend', 'evalue', 'bitscore']
+    yield header
+    hmm = {h:[] for h in header}
+    for line in blast:
+        if line.startswith('#'):
+            continue
+        line = line.strip().split('\t')
+        # Evalue and Bitscore thresholds
+        evalue, bit = float(line[10]), float(line[11])
+        if evalueT is not False and evalue > evalueT:
+            continue
+        if bitT is not False and bit < bitT:
+            continue
+        for i, h in zip(line, header):
+            hmm[h].append(i)
+    hmm = pd.DataFrame(hmm)
+    hmm['evalue'] = [float(i) for i in hmm['evalue']]
+    for query, df in hmm.groupby(by = ['#query']):
+        df = df.sort_values(by = ['evalue'], ascending = True)
+        for hit in df[header].values[0:numHits]:
+            yield hit
+
+def numBlast(blast, numHits, evalueT = False, bitT = False, sort = False):
+    """
+    parse b6 output
+    """
+    if sort is True:
+        for hit in numBlast_sort(blast, numHits, evalueT, bitT):
+            yield hit
+        return
     header = ['#query', 'target', 'pident', 'alen', 'mismatch', 'gapopen',
               'qstart', 'qend', 'tstart', 'tend', 'evalue', 'bitscore']
     yield header
@@ -49,10 +83,49 @@ def numBlast(blast, numHits, evalueT = False, bitT = False):
     for hit in top_hits(hits, numHits, 10, False):
         yield hit
 
-def numDomtblout(domtblout, numHits, evalueT, bitT):
+def numDomtblout_sort(domtblout, numHits, evalueT, bitT):
+    """
+    parse hmm domain table output with sorting
+    """
+    header = ['#target name', 'target accession', 'tlen',
+              'query name', 'query accession', 'qlen',
+              'full E-value', 'full score', 'full bias',
+              'domain #', '# domains',
+              'domain c-Evalue', 'domain i-Evalue', 'domain score', 'domain bias',
+              'hmm from', 'hmm to', 'seq from', 'seq to', 'env from', 'env to',
+              'acc', 'target description']
+    yield header
+    hmm = {h:[] for h in header}
+    for line in domtblout:
+        if line.startswith('#'):
+            continue
+        line = line.strip().split()
+        # domain c-Evalue and domain score thresholds
+        evalue, bit = float(line[11]), float(line[13])
+        if evalueT is not False and evalue > evalueT:
+            continue
+        if bitT is not False and bit < bitT:
+            continue
+        line, desc = line[0:22], ' '.join(line[22:])
+        line.append(desc)
+        for i, h in zip(line, header):
+            hmm[h].append(i)
+    hmm = pd.DataFrame(hmm)
+    hmm['domain c-Evalue'] = [float(i) for i in hmm['domain c-Evalue']]
+    for query, df in hmm.groupby(by = ['#target name', 'domain #']):
+        df = df.sort_values(by = ['domain c-Evalue'], ascending = True)
+        for hit in df[header].values[0:numHits]:
+            yield hit
+
+def numDomtblout(domtblout, numHits, evalueT, bitT, sort):
     """
     parse hmm domain table output
+    this version is faster but does not work unless the table is sorted
     """
+    if sort is True:
+        for hit in numDomtblout_sort(domtblout, numHits, evalueT, bitT):
+            yield hit
+        return
     header = ['#target name', 'target accession', 'tlen',
               'query name', 'query accession', 'qlen',
               'full E-value', 'full score', 'full bias',
@@ -92,10 +165,48 @@ def numDomtblout(domtblout, numHits, evalueT, bitT):
     for hit in top_hits(hits, numHits, 11, False):
         yield hit
 
-def numTblout(tblout, numHits, evalueT, bitT):
+def numTblout_sort(tblout, numHits, evalueT, bitT):
+    """
+    parse hmm hmm table output with sorting
+    """
+    header = ['#target name', 'target accession',
+              'query name', 'query accession',
+              'full E-value', 'full score',  'full bias',
+              'best E-value', 'best score',  'best bias',
+              'exp', 'reg', 'clu',  'ov', 'env', 'dom', 'rep', 'inc',
+              'description of target']
+    yield header
+    hmm = {h:[] for h in header}
+    for line in tblout:
+        if line.startswith('#'):
+            continue
+        line = line.strip().split()
+        # domain full Evalue and full score thresholds
+        evalue, bit = float(line[4]), float(line[5])
+        if evalueT is not False and evalue > evalueT:
+            continue
+        if bitT is not False and bit < bitT:
+            continue
+        line, desc = line[0:18], ' '.join(line[18:])
+        line.append(desc)
+        for i, h in zip(line, header):
+            hmm[h].append(i)
+    hmm = pd.DataFrame(hmm)
+    hmm['full E-value'] = [float(i) for i in hmm['full E-value']]
+    for query, df in hmm.groupby(by = ['#target name']):
+        df = df.sort_values(by = ['full E-value'], ascending = True)
+        for hit in df[header].values[0:numHits]:
+            yield hit
+
+def numTblout(tblout, numHits, evalueT, bitT, sort):
     """
     parse hmm table output
+    this version is faster but does not work unless the table is sorted
     """
+    if sort is True:
+        for hit in numTblout_sort(tblout, numHits, evalueT, bitT):
+            yield hit
+        return
     header = ['#target name', 'target accession',
               'query name', 'query accession',
               'full E-value', 'full score',  'full bias',
@@ -150,6 +261,9 @@ if __name__ == '__main__':
     parser.add_argument(\
             '-f', default = 'b6', type = str,\
             help = 'format (default = b6, options: b6, domtblout, tblout)')
+    parser.add_argument(\
+            '--sort', action = 'store_true', \
+            help = 'sort hits by query name (evalues are sorted by default)')
     args = vars(parser.parse_args())
     # check if file is from stdin
     if args['i'] == '-':
@@ -157,13 +271,16 @@ if __name__ == '__main__':
     else:
         args['i'] = open(args['i'])
     if args['f'] == 'b6':
-        for hit in numBlast(args['i'], args['n'], args['e'], args['b']):
+        for hit in numBlast(args['i'], args['n'], args['e'], args['b'],
+                args['sort']):
             print('\t'.join([str(i) for i in hit]))
     elif args['f'] == 'domtblout':
-        for hit in numDomtblout(args['i'], args['n'], args['e'], args['b']):
+        for hit in numDomtblout(args['i'], args['n'], args['e'], args['b'],
+                args['sort']):
             print('\t'.join([str(i) for i in hit]))
     elif args['f'] == 'tblout':
-        for hit in numTblout(args['i'], args['n'], args['e'], args['b']):
+        for hit in numTblout(args['i'], args['n'], args['e'], args['b'],
+                args['sort']):
             print('\t'.join([str(i) for i in hit]))
     else:
         print('unsupported format:', args['f'])
